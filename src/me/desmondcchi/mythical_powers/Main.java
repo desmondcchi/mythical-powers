4d95d413fd15326d454712f252d3530c5bc390c4
Desmond Chi <dcchi@live.com>
2022-09-10 15:26:12 -0700

    Added /fireball.

--- src/me/desmondcchi/mythical_powers/Main.java
+++ src/me/desmondcchi/mythical_powers/Main.java
@@ -2,12 +2,15 @@ package me.desmondcchi.mythical_powers;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.desmondcchi.mythical_powers.commands.CommandFireball;
 import me.desmondcchi.mythical_powers.commands.Lightning;
 
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		// Commands
 		this.getCommand("lightning").setExecutor(new Lightning());
+		this.getCommand("fireball").setExecutor(new CommandFireball());
 	}
 	
 	@Override
