92c210a9794356edaff3ab2b04cfadf9b1d94a7d
Desmond Chi <dcchi@live.com>
2022-09-10 12:17:01 -0700

    First commit.

---
+++ src/me/desmondcchi/mythical_powers/Main.java
@@ -0,0 +1,15 @@
+package me.desmondcchi.mythical_powers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
